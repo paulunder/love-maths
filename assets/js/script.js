136d5f527e54d3bf1ef21eaa4c2799231fc9d269
paul <paul@edda.work>
2024-02-11 17:52:04 +0100

    implemented addition feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,22 +5,31 @@ document.addEventListener("DOMContentLoaded", function () {
     for (let button of buttons) {
         button.addEventListener("click", function () {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+                //alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                //alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
+
     }
-}
-);
+    runGame("addition")
+});
 
 
 
-function runGame() {
+function runGame(gameType) {
 
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 
 
 }
@@ -43,8 +52,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 }
 
 function displaySubtractQuestion() {
